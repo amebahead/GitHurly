cf6cb23ec7750f9dee6b8c38c8b9f4f383df7771
MacDole <>
2022-03-31 11:09:12 +0900

    feat(settings): 기본 프로젝트 구조 생성

---
+++ GitHurly/Controllers/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  GitHurly
+//
+//  Created by MacDole on 2022/03/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = .green
+        print("hello world")
+    }
+
+
+}
+
